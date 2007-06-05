73feb3d7e5cd6d8c635d69df21e9e1bf75b417d5
dhaun <dhaun@localhost>
2007-06-05 10:49:45 +0000

    Script to create a shipable Geeklog 1.x tarball

---
+++ ship/mkdist.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+#
+# mkdist.sh - make a Geeklog distribution from a local CVS copy
+#
+# Usage: mkdist.sh new-version old-version [german]
+# e.g. ./mkdist.sh 1.3.9rc1 1.3.8-1sr4
+#
+# $Id: mkdist.sh,v 1.1.1.1 2007/06/05 10:49:45 dhaun Exp $
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 new-version old-version [german]"
+  exit
+fi
+
+if [ -z "$2" ]; then
+  echo "Need another version number ..."
+  exit
+fi
+
+if [ -z "$3" ]; then
+  langv="english"
+else
+  langv="german"
+fi
+
+NEWVERSION="geeklog-$1"
+OLDVERSION="geeklog-$2"
+
+cd dist
+
+if [ -d $NEWVERSION ]; then
+  rm -rf $NEWVERSION
+fi
+
+cp -r -p ../Geeklog-1.x $NEWVERSION
+cp -r ../pear-1.3/pear/* $NEWVERSION/system/pear/
+
+cd $NEWVERSION
+
+# remove old default themes
+rm -rf public_html/layout/Classic
+rm -rf public_html/layout/clean
+rm -rf public_html/layout/Digital_Monochrome
+rm -rf public_html/layout/gameserver
+rm -rf public_html/layout/Smooth_Blue
+rm -rf public_html/layout/XSilver
+rm -rf public_html/layout/Yahoo
+
+# no PDF support for now
+rm public_html/pdfgenerator.php
+rm -rf public_html/layout/professional/pdfgenerator
+rm -rf pdfs
+
+# don't ship MT-Blacklist modules any more
+rm -f plugins/spamx/MTBlackList.Examine.class.php
+rm -f plugins/spamx/Import.Admin.class.php
+# only used by the Import class
+rm -rf plugins/spamx/magpierss
+
+# don't ship these language files any more
+rm -f language/chinese_big5.php
+rm -f language/chinese_gb2312.php
+
+find . -type f -name '.*' -exec rm \{\} \;
+find . -name CVS -exec rm -r \{\} \; 2>/dev/null
+
+find . -type f -exec chmod a-x \{\} \;
+chmod a+x emailgeeklogstories
+
+# set the default permissions ...
+chmod -R 775 backups
+chmod -R 775 data
+chmod -R 775 logs
+# chmod -R 775 pdfs
+chmod -R 775 public_html/backend
+chmod -R 775 public_html/images/articles
+chmod -R 775 public_html/images/topics
+chmod -R 775 public_html/images/userphotos
+
+if [ "$langv" = "german" ]; then
+  echo "Making German version ..."
+  rm -rf public_html/docs
+  mv public_html/docs.german public_html/docs
+  mv config.php.german config.php
+  mv sql/mysql_tableanddata.php.german sql/mysql_tableanddata.php
+  mv sql/updates/mysql_1.3.8_to_1.3.9.php.german sql/updates/mysql_1.3.8_to_1.3.9.php
+  mv sql/updates/mysql_1.3.9_to_1.3.10.php.german sql/updates/mysql_1.3.9_to_1.3.10.php
+  mv public_html/admin/install/success.php.german public_html/admin/install/success.php
+else
+  echo "Making English version ..."
+  rm -rf public_html/docs.german
+  rm -f config.php.german
+  rm -f sql/mysql_tableanddata.php.german
+  rm -f sql/updates/mysql_1.3.8_to_1.3.9.php.german
+  rm -f sql/updates/mysql_1.3.9_to_1.3.10.php.german
+  rm -f public_html/admin/install/success.php.german
+fi
+
+cd ..
+
+diff -b -B --brief --recursive -N $OLDVERSION $NEWVERSION | grep -v 'system.pear' | cut -f 4 -d' ' >changed-files
+# diff -b -B --brief --recursive -N $OLDVERSION $NEWVERSION | grep -v layout | grep -v docs | cut -f 4 -d' ' >changed-files
+mv changed-files $NEWVERSION/public_html/docs/
+
+if [ -f $NEWVERSION.tar ]; then
+  rm $NEWVERSION.tar
+fi
+
+tar cf $NEWVERSION.tar $NEWVERSION
+
+if [ -f $NEWVERSION.tar.gz ]; then
+  rm $NEWVERSION.tar.gz
+fi
+
+gzip $NEWVERSION.tar
+
+md5 $NEWVERSION.tar.gz
+
