298ee48fdf39ba9b9b7f1e58a0c02afb8717222a
Dirk Haun <dhaun@users.noreply.github.com>
2009-03-08 18:26:17 +0100

    Added project.geeklog.net cronjobs

---
+++ cronjobs/nightly.sh
@@ -0,0 +1,84 @@
+#!/usr/local/bin/bash
+#
+# nightly.sh - create nightly tarball for Geeklog
+#
+# Also see the mkdist.sh script - some pieces have been copied from there.
+#
+# To be called from a cronjob, hence the use of complete paths in some places.
+
+# do this once:
+# mkdir /usr/home/geeklog2/nightly
+# cd /usr/home/geeklog2/nightly
+# hg clone http://project.geeklog.net/cgi-bin/hgwebdir.cgi/geeklog/ geeklog
+
+cd /usr/home/geeklog2/nightly/geeklog-nightly
+
+# update repository
+/usr/local/bin/hg -q pull > /dev/null 2>&1
+/usr/local/bin/hg -q up > /dev/null 2>&1
+
+# fix config names
+mv db-config.php.dist db-config.php
+mv public_html/siteconfig.php.dist public_html/siteconfig.php
+
+# add PEAR classes
+cd system/pear
+cp /usr/www/users/geeklog2/www/nightly/geeklog-pear.tar.gz .
+tar xfz geeklog-pear.tar.gz
+rm -f geeklog-pear.tar.gz
+cd ../..
+
+# remove unused files
+
+# Blaine's test file ...
+rm -f public_html/blaine.php
+
+# don't ship MT-Blacklist modules any more
+rm -f plugins/spamx/MTBlackList.Examine.class.php
+rm -f plugins/spamx/Import.Admin.class.php
+# only used by the Import class
+rm -rf plugins/spamx/magpierss
+rm -f plugins/spamx/rss.inc.php
+# you'd need to set up a honeypot to use it
+rm -f plugins/spamx/ProjectHoneyPot.Examine.class.php
+# about time we clean up the install directory ...
+rm -f public_html/admin/install/addindex.php
+rm -f system/lib-custom.php.dist
+
+# PEAR buildpackage files
+rm -f plugins/calendar/buildpackage.php
+rm -f plugins/links/buildpackage.php
+rm -f plugins/polls/buildpackage.php
+rm -f plugins/spamx/buildpackage.php
+rm -f plugins/staticpages/buildpackage.php
+rm -rf system/build
+
+# fix permissions
+find . -type f -exec chmod a-x \{\} \;
+chmod a+x emailgeeklogstories
+
+# set the default permissions
+chmod 775 backups
+chmod 775 data
+chmod 775 logs
+chmod 664 logs/*.log
+chmod 775 public_html/backend
+chmod 644 public_html/backend/*.rss
+chmod 775 public_html/images/articles
+chmod 664 public_html/images/articles/*
+chmod 775 public_html/images/topics
+chmod 664 public_html/images/topics/*
+chmod 775 public_html/images/userphotos
+chmod 664 public_html/images/userphotos/*
+
+cd ..
+
+tar cf geeklog-nightly.tar '--exclude=\.hg' geeklog-nightly
+gzip geeklog-nightly.tar
+
+# rename config files back to their names in the repository
+mv geeklog-nightly/db-config.php geeklog-nightly/db-config.php.dist
+mv geeklog-nightly/public_html/siteconfig.php geeklog-nightly/public_html/siteconfig.php.dist
+
+mv geeklog-nightly.tar.gz /usr/www/users/geeklog2/www/nightly/
+
