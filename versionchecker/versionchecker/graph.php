41555bb5ecb588f0239306411c8b625a5ea4e941
Dirk Haun <dirk@haun-online.de>
2011-01-19 20:06:01 +0100

    Added GPLv2+ header and copyright information

--- versionchecker/versionchecker/graph.php
+++ versionchecker/versionchecker/graph.php
@@ -1,4 +1,34 @@
 <?php
+
+/* Reminder: always indent with 4 spaces (no tabs). */
+// +---------------------------------------------------------------------------+
+// | Geeklog Version Checker                                                   |
+// +---------------------------------------------------------------------------+
+// | graph.php                                                                 |
+// |                                                                           |
+// | Graphics library.                                                         |
+// +---------------------------------------------------------------------------+
+// | Copyright (C) 2011 by the following authors:                              |
+// |                                                                           |
+// | Authors: Rouslan Placella  - rouslan AT placella DOT com>                 |
+// +---------------------------------------------------------------------------+
+// |                                                                           |
+// | This program is free software; you can redistribute it and/or             |
+// | modify it under the terms of the GNU General Public License               |
+// | as published by the Free Software Foundation; either version 2            |
+// | of the License, or (at your option) any later version.                    |
+// |                                                                           |
+// | This program is distributed in the hope that it will be useful,           |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+// | GNU General Public License for more details.                              |
+// |                                                                           |
+// | You should have received a copy of the GNU General Public License         |
+// | along with this program; if not, write to the Free Software Foundation,   |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
+// |                                                                           |
+// +---------------------------------------------------------------------------+
+
 // this file can't be used on its own
 if (strpos(strtolower($_SERVER['PHP_SELF']), 'graph.php') !== false) {
     die('This file cannot be used on its own!');
