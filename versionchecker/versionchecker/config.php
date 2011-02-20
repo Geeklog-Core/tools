31c9fb1f0c4a8a2898dbef84e4e91ef81c9e3d5e
Dirk Haun <dirk@haun-online.de>
2011-02-20 19:27:13 +0100

    Geeklog 1.7.2 is the current version now

--- versionchecker/versionchecker/config.php
+++ versionchecker/versionchecker/config.php
@@ -27,7 +27,8 @@ $releases = array (
     array('version' => '1.6.1sr2',   'date' => '02-Jan-11', 'supported' => true,  'upgrade' => ''),
     array('version' => '1.7.0',      'date' => '01-Jul-10', 'supported' => true,  'upgrade' => ''),
     array('version' => '1.7.1',      'date' => '31-Oct-10', 'supported' => true,  'upgrade' => ''),
-    array('version' => '1.7.1sr1',   'date' => '02-Jan-11', 'supported' => true,  'upgrade' => '')
+    array('version' => '1.7.1sr1',   'date' => '02-Jan-11', 'supported' => true,  'upgrade' => ''),
+    array('version' => '1.7.2',      'date' => '20-Feb-11', 'supported' => true,  'upgrade' => '')
 );
 
 // Current stable version
