f29059b9620304029b4b5486727ffd3bf13add98
Dirk Haun <dirk@haun-online.de>
2012-12-31 09:12:40 +0100

    updated to include 1.8.2 release

--- versionchecker/versionchecker/config.php
+++ versionchecker/versionchecker/config.php
@@ -20,16 +20,18 @@ $releases = array (
     array('version' => '1.5.2sr5',   'date' => '30-Jul-09', 'supported' => false, 'upgrade' => ''),
     array('version' => '1.5.2sr6',   'date' => '09-May-10', 'supported' => false, 'upgrade' => ''),
     array('version' => '1.6.0',      'date' => '19-Jul-09', 'supported' => false, 'upgrade' => ''),
-    array('version' => '1.6.0sr1',   'date' => '30-Jul-09', 'supported' => true,  'upgrade' => ''),
-    array('version' => '1.6.0sr2',   'date' => '30-Aug-09', 'supported' => true,  'upgrade' => ''),
-    array('version' => '1.6.1',      'date' => '22-Nov-09', 'supported' => true,  'upgrade' => '1.6.1sr2'),
-    array('version' => '1.6.1sr1',   'date' => '09-May-10', 'supported' => true,  'upgrade' => ''),
-    array('version' => '1.6.1sr2',   'date' => '02-Jan-11', 'supported' => true,  'upgrade' => ''),
+    array('version' => '1.6.0sr1',   'date' => '30-Jul-09', 'supported' => false,  'upgrade' => ''),
+    array('version' => '1.6.0sr2',   'date' => '30-Aug-09', 'supported' => false,  'upgrade' => ''),
+    array('version' => '1.6.1',      'date' => '22-Nov-09', 'supported' => false,  'upgrade' => '1.6.1sr2'),
+    array('version' => '1.6.1sr1',   'date' => '09-May-10', 'supported' => false,  'upgrade' => ''),
+    array('version' => '1.6.1sr2',   'date' => '02-Jan-11', 'supported' => false,  'upgrade' => ''),
     array('version' => '1.7.0',      'date' => '01-Jul-10', 'supported' => true,  'upgrade' => ''),
     array('version' => '1.7.1',      'date' => '31-Oct-10', 'supported' => true,  'upgrade' => ''),
     array('version' => '1.7.1sr1',   'date' => '02-Jan-11', 'supported' => true,  'upgrade' => ''),
     array('version' => '1.7.2',      'date' => '20-Feb-11', 'supported' => true,  'upgrade' => ''),
-    array('version' => '1.8.0',      'date' => '12-Jun-11', 'supported' => true,  'upgrade' => '')
+    array('version' => '1.8.0',      'date' => '12-Jun-11', 'supported' => true,  'upgrade' => ''),
+    array('version' => '1.8.1',      'date' => '09-Oct-11', 'supported' => true,  'upgrade' => ''),
+    array('version' => '1.8.2',      'date' => '30-Dec-12', 'supported' => true,  'upgrade' => '')
 );
 
 // Current stable version
