4b699631edbb57b32b29441eb808775429b5adbf
Dirk Haun <dirk@haun-online.de>
2015-10-28 21:20:12 +0100

    rescued from geeklog.net: a script that tried to make use of spam blacklists used by some wikis

---
+++ defunct/spam-merge/entryrem.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# Add an entry to be removed, RDF formatted.
+#
+# Call like this: ./entryrem.sh <entry> <feed>
+#
+# Part of the larger "update.sh" script (see there for details)
+#
+# written 2005-03-30 by Dirk Haun <dirk AT haun-online DOT de>
+
+entry=$1
+feed=$2
+
+about='http://www.usemod.com/cgi-bin/mb.pl?SharedAntiSpam'
+link='http://www.usemod.com/cgi-bin/mb.pl?SharedAntiSpam'
+timestamp=`date +%FT%T%z | cut -c-22 | sed 's/\(.*\)$/\1:00/'`
+author='Geeklog'
+
+cat > $feed.tmp <<HERE
+<item rdf:about="$about">
+<title>$entry</title>
+<link>$link</link>
+<description>$entry...</description>
+<content:encoded><![CDATA[[Blacklist deletions] <p>$entry</p>
+]]></content:encoded>
+<dc:subject>Blacklist deletions</dc:subject>
+<dc:creator>$author</dc:creator>
+<dc:date>$timestamp</dc:date>
+</item>
+HERE
+
+cat $feed >> $feed.tmp
+mv $feed.tmp $feed
+
